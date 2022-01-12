a414380cbcbc63ec6d59e32d0dff6e8f954eb2b8
dragana1611 <dragana16111@gmail.com>
2022-01-12 21:36:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("https://orangevalleycaa.org/api/videos").then(
+        (response) => response.json()
+      );
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="Header">Videos</h1>
       </header>
+      {data.map((video) => (
+        <div key={video.id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls src={video.video_url} />
+          <div class="h-divider">
+            <div class="shadow"></div>
+            <div class="text">
+              <i>Art Video</i>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
